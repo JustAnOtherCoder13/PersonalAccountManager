378044299e4f239202c5a054a9d26d17c4a72acd
JustAnOtherCoder13 <justanothercoder13@gmail.com>
2021-09-13 17:16:40 +0200

    change package from 'example' to 'piconemarc' add 'core' and 'viewmodel' module add needed packages in each module update gradle

--- app/src/main/java/com/piconemarc/personalaccountmanager/ui/theme/Theme.kt
+++ app/src/main/java/com/piconemarc/personalaccountmanager/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.personalaccountmanager.ui.theme
+package com.piconemarc.personalaccountmanager.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
